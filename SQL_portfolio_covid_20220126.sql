55b5f274407dca4c2f715459d198612ecdd2c4bf
Phil Chen <85400487+philchenda@users.noreply.github.com>
2022-01-26 19:47:01 -0800

    Update SQL_portfolio_covid_20220126.sql

--- SQL_portfolio_covid_20220126.sql
+++ SQL_portfolio_covid_20220126.sql
@@ -121,3 +121,6 @@ Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
+
+Select *
+From PercentPopulationVaccinated
